c532bccae47c444181c40275f6eb4cfb3704692c
Enriquepardo <epardser@myuax.com>
2023-03-30 23:49:00 +0200

    mas cambios

--- Juego Pokemon/pokemon.py
+++ Juego Pokemon/pokemon.py
@@ -1,31 +1,86 @@
-
-import random
+import random 
 
 
 class Pokemon:
-    ID = []
-    salud_max = 100
-    salud_min = 0
-    ataque_max = 10
-    ataque_min = 1
-    defensa_max = 10
-    defensa_min = 1
-    tipos_arma = ["Puñetazo", "Patada", "Codazo", "Cabezazo"]
+    pokemon_ids = []
 
-    def __init__(self, ID,nombre,tipo_arma,puntos_salud,indice_ataque,indice_defensa):
-        self.ID = ID
-        self.nombre = nombre
-        self.tipo_arma = tipo_arma
-        self.puntos_salud = puntos_salud
-        self.indice_ataque = indice_ataque
-        self.indice_defensa = indice_defensa
+    def __init__(self, id, nombre, arma,puntos_salud,indice_ataque,indice_defensa):
+        if not isinstance(id, int):
+            raise TypeError('ID tiene que ser un numero entero')
+        if id in Pokemon.pokemon_ids:
+            raise ValueError('ID ya existe para otro pokemon')
+        if not isinstance(nombre, str):
+            raise TypeError('El nombre debe ser una cadena de caracteres')
+        if not isinstance(arma, str):
+            raise TypeError('El arma debe ser una cadena de caractere')
+        if not isinstance(puntos_salud, int):
+            raise TypeError('Los puntos de salud deben ser un numero entero')
+        if not isinstance(indice_ataque, int):
+            raise TypeError('El indice de ataque debe ser un numero entero')
+        if not isinstance(indice_defensa, int):
+            raise TypeError('El indice de defensa debe ser un numero entero')
+
+        self.__id = id
+        self.__nombre = nombre
+        self.__arma = arma
+        self.__puntos_salud = puntos_salud
+        self.__indice_ataque = indice_ataque
+        self.__indice_defensa = indice_defensa
+
+        Pokemon.pokemon_ids.append(id)
 
 
-    def enum_arma(self):
-        return list(enumerate(self.tipos_arma))
+    def __str__(self):
+        return ' Pokemon ID {} con nombre {} usa de arma {} y tiene {} puntos de salud' .format(self.__id, self.__nombre, self.__arma, self.__puntos_salud)
+    
+    def __del__(self):
+        Pokemon.pokemon_ids.remove(self.__id)
 
 
+    def get_id(self):
+        return self.__id
+    
+    def get_nombre(self):    
+        return self.__nombre
+    
+    def get_arma(self):
+        return self.__arma
+    
+    def get_puntos_salud(self):
+        return self.__puntos_salud
+    
+    def get_indice_ataque(self):
+        return self.__indice_ataque
+    
+    def get_indice_defensa(self):
+        return self.__indice_defensa
+
+    # solo se pueden modificar los puntos de salud
+    def set_puntos_salud(self, puntos_salud):
+        if not isinstance(puntos_salud, int):
+            raise TypeError('Los puntos de salud deben ser un numero entero')
+        self.__puntos_salud = puntos_salud
 
 
+    def is_alive(self):
+        if self.__puntos_salud > 0:
+            return True
+        else:
+            return False
+    
+    def pelea_atacar(self, pokemon_objetivo):
+        if not isinstance(pokemon_objetivo, Pokemon):
+            raise TypeError('El pokemon objetivo debe ser una instancia de la clase Pokemon')
+        else:
+            if pokemon_objetivo.is_alive():
+                pokemon_objetivo.pelea_defender(self.__indice_ataque)
+
+    def pelea_defender(self, puntos_daño):
+    
+        if self.__indice_defensa > puntos_daño:
+            return 0
+        else:
+            self.__puntos_salud -= (puntos_daño - self.__indice_defensa)
+            return True
 
 
